55cba304758776a5e1591d62a00de3d82ac42316
corinnebaudens <corinne.jankowski@laposte.net>
2015-12-26 18:16:35 +0100

    IHM version1

---
+++ src/Main/Main.java
@@ -0,0 +1,29 @@
+package Main;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+//import vues.Fenetre;
+import vues.Fenetre2;
+import vues.Demarrage;
+
+public class Main {
+	public static void main(String[] args)
+	   {
+		//public void run()
+		
+			Demarrage f = new Demarrage();
+	      //JFrame fen= new JFrame("Traitement de Séries chronologiques boursières");
+	      //Fenetre2 pan = new Fenetre2();
+	      //fen.setContentPane(pan);
+	      //fen.setPreferredSize(new Dimension(800,600));
+	      //f.setTitle();
+	      //fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      //fen.setVisible(true);
+	      //fen.pack();
+	      //fen.setLocationRelativeTo(null);
+	      //fen.setResizable(true);
+	   }
+	
+}
