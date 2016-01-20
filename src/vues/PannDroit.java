83eba00cfaf6ecbb3b544592c17a814ec6a6719d
GregoireDurand <gregoire.durand@gmail.com>
2016-01-20 13:27:45 +0100

    méthode dessiner dans classse PannDroit

--- src/vues/PannDroit.java
+++ src/vues/PannDroit.java
@@ -17,24 +17,31 @@ import modeles.Modele;
 
 public class PannDroit {
 	JPanel panD;
+	Border bd;
 	
 	public PannDroit(Modele modele, Controleur controleur, JFrame fen, JPanel panD){
 		
-    AffGraphique graphique = new AffGraphique(modele, controleur);
-    Border EtchedBorderbd = BorderFactory.createEtchedBorder();
-    Border bd = BorderFactory.createTitledBorder(EtchedBorderbd,"Résultat graphique",
-  		  TitledBorder.LEFT, TitledBorder.TOP,new Font("Arial", Font.BOLD,13),Color.BLUE);
-    graphique.setBorder(bd);
-//    scrollGraph.setPreferredSize(new Dimension(450,500));
-    panD = new JPanel();
-    panD.setPreferredSize(new Dimension(450,500));
-   //panD.setLayout(new FlowLayout());
-    panD.add(graphique);
-    panD.setOpaque(false);
-    this.panD=panD;
-   
+//		AffGraphique graphique = new AffGraphique(modele, controleur);
+		Border EtchedBorderbd = BorderFactory.createEtchedBorder();
+	    bd = BorderFactory.createTitledBorder(EtchedBorderbd,"Résultat graphique",
+	    		TitledBorder.LEFT, TitledBorder.TOP,new Font("Arial", Font.BOLD,13),Color.BLUE);
+//		graphique.setBorder(bd);
+//		scrollGraph.setPreferredSize(new Dimension(450,500));
+	    panD = new JPanel();
+	    panD.setPreferredSize(new Dimension(450,500));
+//		panD.setLayout(new FlowLayout());
+//		panD.add(graphique);
+
+	    panD.setOpaque(false);
+	    this.panD=panD;
 	}
 
+	public void dessiner(Modele modele, Controleur controleur){
+		AffGraphique graphique = new AffGraphique(modele, controleur);
+		graphique.setBorder(bd);
+		panD.add(graphique);
+	}
+	
 	public JPanel getPanD() {
 		return panD;
 	}
